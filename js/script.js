1755a3393413846904ce3308e430fa05f83018ae
RISHABH DEV <53978955+rishabhdev444@users.noreply.github.com>
2024-02-11 06:40:22 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-let currentSong=new Audio("/songs/");
+let currentSong=new Audio("./songs/");
 let songs;
 let currFolder;
 
@@ -86,7 +86,7 @@ async function displayAlbums() {
     for (let index = 0; index <array.length; index++) {
         const e = array[index];
         if (e.href.includes("/songs/") && !e.href.includes(".htaccess")) {
-            let folder = e.href.split("/").slice(-2)[0]
+            let folder = e.href.split("/").slice(-2)[1]
             // Get the metadata of the folder
             let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json(); 
